02ee8c25b729c42a7f18ca21084dd7c240912aff
Christoffer Nordlander <christoffernordlander02@gmail.com>
2024-05-25 15:43:06 +0200

    Added so that the script is in the readme_handling folder when run

--- readme_handling/update_readme.sh
+++ readme_handling/update_readme.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
+# Change working directory to the directory where the script resides
+cd "$(dirname "$0")"
+
 # Specify the folder containing Markdown files
-markdown_folder="readme_handling/logs"
+markdown_folder="logs"
 
 # Find the newest Markdown files in the folder based on file names
 newest_files=$(ls -1 "$markdown_folder"/*.md | sort -r | head -n 1)
 
 # Save the content of the main README.md
-main_content=$(cat readme_handling/main.md)
+main_content=$(cat main.md)
 
 # Combine the main content and the content of the newest Markdown files into README.md
 echo "$main_content" > ../README.md
