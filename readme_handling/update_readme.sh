9ee6dac2a51e90a5b34f38145aa7952e16b4f687
Christoffer Nordlander <christoffernordlander02@gmail.com>
2024-05-25 15:36:16 +0200

    Updated hook

--- readme_handling/update_readme.sh
+++ readme_handling/update_readme.sh
@@ -7,10 +7,11 @@ markdown_folder="logs"
 newest_files=$(ls -1 "$markdown_folder"/*.md | sort -r | head -n 1)
 
 # Save the content of the main README.md
-main_content=$(cat main.md)
+main_content=$(cat ./readme_handling/main.md)
 
 # Combine the main content and the content of the newest Markdown files into README.md
 echo "$main_content" > ../README.md
+# Adds a space 
 echo  "" >> ../README.md
 for file in $newest_files; do
     cat "$file" >> ../README.md
