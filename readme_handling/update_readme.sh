f7335700d5918c38a4ef18983667b5002f73a958
Christoffer Nordlander <christoffernordlander02@gmail.com>
2024-05-25 15:38:57 +0200

    bruh

--- readme_handling/update_readme.sh
+++ readme_handling/update_readme.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Specify the folder containing Markdown files
-markdown_folder="./readme_handling/logs"
+markdown_folder="readme_handling/logs"
 
 # Find the newest Markdown files in the folder based on file names
 newest_files=$(ls -1 "$markdown_folder"/*.md | sort -r | head -n 1)
 
 # Save the content of the main README.md
-main_content=$(cat ./readme_handling/main.md)
+main_content=$(cat readme_handling/main.md)
 
 # Combine the main content and the content of the newest Markdown files into README.md
 echo "$main_content" > ../README.md
